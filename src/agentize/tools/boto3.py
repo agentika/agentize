bff61e5fffcf4036e1a26786d1bb764f819fd812
johnlin <linton.tw@gmail.com>
2025-05-13 11:49:37 +0800

    feat: file path and date folder

--- src/agentize/tools/boto3.py
+++ src/agentize/tools/boto3.py
@@ -6,6 +6,7 @@ import os
 import time
 from datetime import datetime
 from io import BytesIO
+from pathlib import Path
 
 import boto3
 from agents import function_tool
@@ -30,12 +31,12 @@ HTML_TEMPLATE = """<!doctype html>
 """
 
 
-def _upload_object(content: str, object_name: str) -> bool:
+def _upload_object(content: str, file_name: str) -> bool:
     """Upload a file to an S3 bucket.
 
     Args:
         content(str) : The content to upload.
-        object_name (str): S3 object name. If not specified then unix timestamp is used.
+        file_name (str): file name. If not specified then unix timestamp is used.
 
     Returns:
         bool: True if file was uploaded, else False
@@ -45,14 +46,18 @@ def _upload_object(content: str, object_name: str) -> bool:
     if bucket is None:
         raise ValueError("AWS_BUCKET_NAME environment variable not set")
 
-    # If S3 object_name was not specified, use unix timestamp
-    if object_name is None or object_name == "":
+    # If file_name was not specified, use unix timestamp for object name
+    if file_name is None or file_name == "":
         # Use the current unix timestamp as the object name remove floa<ting point
         timestamp = int(time.time())
         # Convert the timestamp to a string
-        object_name = str(timestamp)
+        file_name = str(timestamp)
     else:
-        object_name = f"{object_name}-{datetime.now().strftime('%m-%d')}"
+        # Remove the file extension from the file name
+        file_name = Path(file_name).stem
+
+    # Use the specified file name. upload path yy-mm-dd/<File Name>
+    object_name = f"{datetime.now().strftime('%y-%m-%d')}/{file_name}"
 
     s3_client = boto3.client("s3")
     try:
@@ -72,12 +77,12 @@ def _upload_object(content: str, object_name: str) -> bool:
     return True
 
 
-def upload_markdown(title: str, content: str, object_name: str) -> bool:
+def upload_markdown(title: str, content: str, file_name: str) -> bool:
     """Upload markdown file to an S3 bucket.
     Args:
         title (str): The title of the page.
         content(str) : The markdown content to upload.
-        object_name (str): S3 object name. If not specified then unix timestamp is used.
+        file_name (str): The file name. If not specified then unix timestamp is used.
 
     Returns:
         bool: True if file was uploaded, else False
@@ -86,7 +91,7 @@ def upload_markdown(title: str, content: str, object_name: str) -> bool:
     safe_md = json.dumps(content)
     # convert markdown to html
     html = HTML_TEMPLATE.format(title=title, md=safe_md)
-    return _upload_object(html, object_name)
+    return _upload_object(html, file_name)
 
 
 upload_markdown_tool = function_tool(upload_markdown)
